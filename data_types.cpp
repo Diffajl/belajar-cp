c7ce87e03a9595d7d5ab9c4ce81c9d5a075a01f0
iffa <ahmaddiffaj@gmail.com>
2025-03-03 06:25:31 +0700

    menambahkan file inputs

---
+++ data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 3;
+    string name = "John";
+    float pi = 3.14;
+    char f = 'f';
+    double x = 3.3;
+    const float euler = 2.71;
+    cout << "n: " << n << endl; 
+    cout << "name: " << name << endl; 
+    cout << "pi: " << pi << endl; 
+    cout << "f: " << f << endl; 
+    cout << "euler: " << euler << endl; 
+}
